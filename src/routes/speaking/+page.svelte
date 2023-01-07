b3419b476401c9d8398ac17f863addeef3fcee3c
Fergus Leahy <ferg@outverse.com>
2023-01-07 12:14:50 +0000

    initial version of blog

---
+++ src/routes/speaking/+page.svelte
@@ -0,0 +1,263 @@
+<main>
+  <div class="sm:px-8 mt-16 sm:mt-32">
+    <div class="mx-auto max-w-7xl lg:px-8">
+      <div class="relative px-4 sm:px-8 lg:px-12">
+        <div class="mx-auto max-w-2xl lg:max-w-5xl">
+          <header class="max-w-2xl">
+            <h1
+              class="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl"
+            >
+              Stuff I've said, somewhere...
+            </h1>
+            <p class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+              I recently talked about progressively moving our React code base to Svelte + Kit, part
+              one of a series that will demonstrate the process of moving a fast moving in-use code
+              base.
+              <br /><br />
+              I enjoy sharing what I'm working on and have previously given talks about my PhD at
+              various academic conferences.
+            </p>
+          </header>
+          <div class="mt-16 sm:mt-20">
+            <div class="space-y-20">
+              <section
+                aria-labelledby=":r0:"
+                class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
+              >
+                <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+                  <h2 id=":r0:" class="text-sm font-semibold text-zinc-800 dark:text-zinc-100">
+                    Talks
+                  </h2>
+                  <div class="md:col-span-3">
+                    <div class="space-y-16">
+                      <article class="group relative flex flex-col items-start">
+                        <h3
+                          class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100"
+                        >
+                          <div
+                            class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+                          ></div>
+                          <a
+                            href="https://www.youtube.com/watch?v=DXQl1G54DJY&t=2513s"
+                            target="_blank"
+                            rel="noreferrer"
+                            ><span
+                              class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"
+                            ></span
+                            ><span class="relative z-10"
+                              >Moving from React to Svelte, progressively.</span
+                            ></a
+                          >
+                        </h3>
+                        <p
+                          class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+                        >
+                          <span
+                            class="absolute inset-y-0 left-0 flex items-center"
+                            aria-hidden="true"
+                            ><span
+                              class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"
+                            ></span></span
+                          >SvelteSociety London, Nov 2022
+                        </p>
+                        <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                          How we're progressively moving an actively worked on codebase (with 4
+                          engineers) into SvelteKit, to enable us to progressively enhance the
+                          frontend with Svelte + Kit whilst minimising impact on product velocity.
+                        </p>
+                        <div
+                          aria-hidden="true"
+                          class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+                        >
+                          Watch video<svg
+                            viewBox="0 0 16 16"
+                            fill="none"
+                            aria-hidden="true"
+                            class="ml-1 h-4 w-4 stroke-current"
+                          >
+                            <path
+                              d="M6.75 5.75 9.25 8l-2.5 2.25"
+                              stroke-width="1.5"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            ></path>
+                          </svg>
+                        </div>
+                      </article>
+                    </div>
+                  </div>
+                </div>
+              </section>
+
+              <!-- <section
+                  aria-labelledby=":r1:"
+                  class="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40"
+                >
+                  <div class="grid max-w-3xl grid-cols-1 items-baseline gap-y-8 md:grid-cols-4">
+                    <h2 id=":r1:" class="text-sm font-semibold text-zinc-800 dark:text-zinc-100">
+                      Podcasts
+                    </h2>
+                    <div class="md:col-span-3">
+                      <div class="space-y-16">
+                        <article class="group relative flex flex-col items-start">
+                          <h3
+                            class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100"
+                          >
+                            <div
+                              class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+                            ></div>
+                            <a href="/speaking#"
+                              ><span
+                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"
+                              ></span
+                              ><span class="relative z-10"
+                                >Using design as a competitive advantage</span
+                              ></a
+                            >
+                          </h3>
+                          <p
+                            class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+                          >
+                            <span
+                              class="absolute inset-y-0 left-0 flex items-center"
+                              aria-hidden="true"
+                              ><span
+                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"
+                              ></span></span
+                            >Encoding Design, July 2022
+                          </p>
+                          <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                            How we used world-class visual design to attract a great team, win over
+                            customers, and get more press for Planetaria.
+                          </p>
+                          <div
+                            aria-hidden="true"
+                            class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+                          >
+                            Listen to podcast<svg
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              aria-hidden="true"
+                              class="ml-1 h-4 w-4 stroke-current"
+                            >
+                              <path
+                                d="M6.75 5.75 9.25 8l-2.5 2.25"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></path>
+                            </svg>
+                          </div>
+                        </article>
+                        <article class="group relative flex flex-col items-start">
+                          <h3
+                            class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100"
+                          >
+                            <div
+                              class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+                            ></div>
+                            <a href="/speaking#"
+                              ><span
+                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"
+                              ></span
+                              ><span class="relative z-10"
+                                >Bootstrapping an aerospace company to $17M ARR</span
+                              ></a
+                            >
+                          </h3>
+                          <p
+                            class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+                          >
+                            <span
+                              class="absolute inset-y-0 left-0 flex items-center"
+                              aria-hidden="true"
+                              ><span
+                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"
+                              ></span></span
+                            >The Escape Velocity Show, March 2022
+                          </p>
+                          <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                            The story of how we built one of the most promising space startups in
+                            the world without taking any capital from investors.
+                          </p>
+                          <div
+                            aria-hidden="true"
+                            class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+                          >
+                            Listen to podcast<svg
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              aria-hidden="true"
+                              class="ml-1 h-4 w-4 stroke-current"
+                            >
+                              <path
+                                d="M6.75 5.75 9.25 8l-2.5 2.25"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></path>
+                            </svg>
+                          </div>
+                        </article>
+                        <article class="group relative flex flex-col items-start">
+                          <h3
+                            class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100"
+                          >
+                            <div
+                              class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+                            ></div>
+                            <a href="/speaking#"
+                              ><span
+                                class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"
+                              ></span
+                              ><span class="relative z-10"
+                                >Programming your company operating system</span
+                              ></a
+                            >
+                          </h3>
+                          <p
+                            class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5"
+                          >
+                            <span
+                              class="absolute inset-y-0 left-0 flex items-center"
+                              aria-hidden="true"
+                              ><span
+                                class="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"
+                              ></span></span
+                            >How They Work Radio, September 2021
+                          </p>
+                          <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                            On the importance of creating systems and processes for running your
+                            business so that everyone on the team knows how to make the right
+                            decision no matter the situation.
+                          </p>
+                          <div
+                            aria-hidden="true"
+                            class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500"
+                          >
+                            Listen to podcast<svg
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              aria-hidden="true"
+                              class="ml-1 h-4 w-4 stroke-current"
+                            >
+                              <path
+                                d="M6.75 5.75 9.25 8l-2.5 2.25"
+                                stroke-width="1.5"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></path>
+                            </svg>
+                          </div>
+                        </article>
+                      </div>
+                    </div>
+                  </div>
+                </section> -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
